3bdd7c682ffb03725f2a276e39d0b03f8be69efa
Sayan Shankhari <34585895+SayanThe01Guy@users.noreply.github.com>
2018-12-30 01:19:01 +0530

    Add files via upload

--- des/des.h
+++ des/des.h
@@ -3,70 +3,146 @@
 
 #include "data.h"
 
-BYTE PI[64] = {
-	0x3a, 0x32, 0x2a, 0x22, 0x1a, 0x12, 0x0a, 0x02,
-	0x3c, 0x34, 0x2c, 0x24, 0x1c, 0x14, 0x0c, 0x04,
-	0x3e, 0x36, 0x2e, 0x26, 0x1e, 0x16, 0x0e, 0x06,
-	0x40, 0x38, 0x30, 0x28, 0x20, 0x18, 0x10, 0x08,
-	0x39, 0x31, 0x29, 0x21, 0x19, 0x11, 0x09, 0x01,
-	0x3b, 0x33, 0x2b, 0x23, 0x1b, 0x13, 0x0b, 0x03,
-	0x3d, 0x35, 0x2d, 0x25, 0x1d, 0x15, 0x0d, 0x05,
-	0x3f, 0x37, 0x2f, 0x27, 0x1f, 0x17, 0x0f, 0x07
-};
-
-BYTE PF[64] = {
-	0x28, 0x08, 0x30, 0x10, 0x38, 0x18, 0x40, 0x20,
-	0x27, 0x07, 0x2f, 0x0f, 0x37, 0x17, 0x3f, 0x1f,
-	0x26, 0x06, 0x2e, 0x0e, 0x36, 0x16, 0x3e, 0x1e,
-	0x25, 0x05, 0x2d, 0x0d, 0x35, 0x15, 0x3d, 0x1d,
-	0x24, 0x04, 0x2c, 0x0c, 0x34, 0x14, 0x3c, 0x1c,
-	0x23, 0x03, 0x2b, 0x0a, 0x33, 0x13, 0x3b, 0x1b,
-	0x22, 0x02, 0x2a, 0x09, 0x32, 0x12, 0x3a, 0x1a,
-	0x21, 0x01, 0x29, 0x08, 0x31, 0x11, 0x39, 0x19
-};
-
-BYTE SB[8][4][16] = {
-	{
-		{14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7},
-		{0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8},
-		{4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0},
-		{15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13}
-	}, {
-		{15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10},
-		{3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5},
-		{0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15},
-		{13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9}
-	}, {
-		{10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8},
-		{13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1},
-		{13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7},
-		{1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12}
-	}, {
-		{7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15},
-		{13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9},
-		{10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4},
-		{3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14}
-	}, {
-		{2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9},
-		{14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6},
-		{4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14},
-		{11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3}
-	}, {
-		{12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11},
-		{10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8},
-		{9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6},
-		{4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13}
-	}, {
-		{4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1},
-		{13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6},
-		{1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2},
-		{6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12}
-	}, {
-		{13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7},
-		{1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2},
-		{7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8},
-		{2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11}
+void byte_to_bits(int n) {
+	int i;
+	for(i = 0; i < n; i++) {
+		_eB[n-1-i] = (_cB >> i) & 0x01;
 	}
-};
+}
+void bits_to_byte(int n) {
+	_cB = 0;
+	int i;
+	for(i = 0; i < n; i++) {
+		_cB = (_cB << 1) | _eB[i];
+	}
+}
+
+void iniPermute() {
+	int i;
+	BYTE X[64];
+	for(i = 0; i < 64; i++)
+		X[i] = _X[i];
+	for(i = 0; i < 64; i++)
+		_X[i] = X[IP[i]];
+}
+
+void finPermute() {
+	int i;
+	BYTE X[64];
+	for(i = 0; i < 64; i++)
+		X[i] = _X[i];
+	for(i = 0; i < 64; i++)
+		_X[i] = X[FP[i]];
+}
+// Generate Key
+void genKey(int r) {
+	int i, x;
+	x = ((r == 0) || (r == 1) || (r == 8) || (r == 15)) ? 1 : 2;
+// Copy to temporary location
+	for(i = 0; i < 28; i++) {
+		_Y[i] = _K[(28 - i - x) % 28];
+		_Y[28 + i] = _K[28 + ((28 - i - x) % 28)]
+	}
+	for(i = 0; i < 48; i++) {
+		_k[i] = _Y[KB[i]];
+	}
+}
+
+void coreDES(int r, BYTE R[]) {
+	int i;
+	BYTE rs, cs, x;
+// right part expansion
+	for(i = 0; i < 48; i++) {
+		_Y[i] = R[EB[i]];
+	}
+// substitution
+	for(i = 0; i < 8; i++) {
+		rs = (_Y[6*i] << 1) | _Y[6*i + 5];
+		cs = 0;
+		for (x = 1; x < 5; x++) {
+			cs = (cs << 1) | _Y[6*i + x];
+		}
+		x = SB[i][rs][cs];
+// copy(R[4*i : 4*i+4], B2b(4,x))
+	}
+	genKey(r);
+// Adding Key
+	for(i = 0; i < 48; i++) {
+		R[i] ^= _k[i];
+	}
+}
+
+// Feistal Round
+void fr(int r) {
+	int i;
+	BYTE L[32], R[32];
+// Filling up L and R
+	for(i = 0; i < 32; i++) {
+		L[i] = _X[i], R[i] = _X[32+i];
+	}
+// Sending R to coreDES
+	coreDES(r, R);
+// Adding R to L
+	for(i = 0; i < 32; i++) {
+		L[i] ^= R[i];
+	}
+// Putting Back in Swapped Position
+	for(i = 0; i < 32; i++) {
+		_X[i] = R[i];
+		_X[32+i] = L[i];
+	}
+// _X updated
+}
+
+void encrypt_des(BYTE P[], BYTE K[]) {
+	int i, j;
+printf("Received Request with P and K as text (8*8)\n");
+printf("Expanding Bytes' to Bits' Blocks => 8*8 --> 1 * 64\n");
+	for(i = 0; i < 8; i++) {
+		_cB = K[i];
+		byte_to_bits(8);
+		for(j = 0; j < 8; j++) {
+			_X[8*i + j] = _eB[j];
+		}
+	}
+printf("Expanded K: \n");
+showArr(_X, 64);
+	j = 0;
+	for(i = 0; i < 64; i++) {
+		if(i%8 != 0)
+			_K[j++] = _X[i];
+	}
+printf("Shortened K: \n");
+showArr(_K, 56);
+printf("Expanding Bytes' to Bits' Blocks => 8*8 --> 1 * 64\n");
+	for(i = 0; i < 8; i++) {
+		_cB = P[i];
+		byte_to_bits(8);
+		for(j = 0; j < 8; j++) {
+			_X[8*i + j] = _eB[j];
+		}
+	}
+printf("Expanded P: \n");
+	showArr(_X, 64);
+printf("Expanded K: \n");
+
+	iniPermute();
+
+printf("Performing 16 feistal rounds\n");
+	for(i = 0; i < 16; i++) {
+		fr(i); // FeistalRound
+	}
+printf("Performing Final Permutation\n");
+	finPermute();
+printf("Shrinking Bits' to Bytes' Blocks => 1 * 64 --> 8 * 8\n");
+	for(i = 0; i < 8; i++) {
+		for(j = 0; j < 8; j++) {
+			_eB[j] = _X[8*i + j];
+		}
+		bits_to_byte(8);
+		P[i] = _cB;
+	}
+printf("Encryption Done\n");
+}
 
 #endif
--- des/main.c
+++ des/main.c
@@ -1,73 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
-/*
 #define BYTE unsigned char
-#define UL unsigned long
-#define N 100
-*/
 
-#include "data.h"
+BYTE X[8], c;
+
+void showArr(BYTE A[], int n) {
+	int i;
+	for(i = 0; i < n; printf("%02x ", A[i++]));
+	printf("\n");
+}
+
 #include "des.h"
 
 int main(int argc, char *argv[]) {
-/*	FILE *fpi, *fpo;
 	int i;
-	if(argc < 3) {
-		perror("Insufficient Data\nUse \"<PROGRAM_NAME> <INPUT_FILE_NAME> <OUTPUT_FILE_NAME> <ENTER>\"");
-		exit(1);
-	}
-	if((fpi=(fopen(argv[1], "r"))) == NULL) {
-		perror("Input file Openning ERROR!...\n");
-		exit(1);
-	}
-	if((fpo=(fopen(argv[2], "w+"))) == NULL) {
-		perror("Output file Openning ERROR!...\n");
-		exit(1);
-	}
-	
-	BYTE *X = (BYTE *)malloc(N);
-	UL nx;
-	BYTE c=0;
-	ssize_t x;
+	BYTE P[8] = {'H', 'E', 'L', 'L', 'O', '!', '\0', '\0'};
+	BYTE K[8] = {'P', 'A', 'S', 'S', 'W', 'O', 'R', 'D'};
+	printf("Hello in Data Encryption Algorithm\n");
+printf("P: ");
+	showArr(P, 8);
+printf("K: ");
+	showArr(K, 8);
 
-	fseek(fpi, 0, SEEK_END);
-	nx = ftell(fpi);
-	fseek(fpi, 0, SEEK_SET);
-
-	for(i=0; i<nx; i++) {
-		x = fread(&c, 1, 1, fpi);
-		X[i] = c;
-	}
-
-//x = fread(&c, 1, 1, fpi);
-//x = fwrite(&c, 1, 1, fpo);
-
-//	dspl(X, nx);
-
-	encrypt(X, &nx, 3);
-//	dspl(X, nx);
-	x = fwrite(X, nx, 1, fpo);
-
-//	decrypt(X, &nx, 3);
-//	dspl(X, nx);
-//	x = fwrite(X, nx, 1, fpi);
-
-	fclose(fpi);
-	fclose(fpo);
-
-	printf("Success!\n");
-*/
-	int i, j;
-/*for(i=0; i<64; printf("0x%02x, ", PI[i++]));
-printf("\n");*/
-//for(i=0; i<64; printf("0x%02x, ", PF[i++]));
-//printf("\n");
-/*for(i=0; i<4; i++) {
-	for(j=0; j<16; printf("0x%02x, ", SB[i][j++]));
-	printf("\n");
-}
-*/
+	encrypt_des(P, K);
+//	decrypt_des(C, K);
+	showArr(P, 8);
 	return 0;
 }
